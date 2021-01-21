19ae48f2eb4b6897a7adaceffe8e1733a03ebe5d
Denis Zyulev <zzulev@gmail.com>
2021-01-21 23:44:01 +0100

    Init app

---
+++ macos/Browserval-macOS/AppDelegate.swift
@@ -0,0 +1,52 @@
+import Foundation
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+  var popover: NSPopover!
+  var bridge: RCTBridge!
+  var statusBarItem: NSStatusItem!
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    print("111111111111111111111111111")
+    let jsCodeLocation: URL
+
+    #if DEBUG
+      print("2222222222222222222222")
+      jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    #else
+      jsCodeLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle")!
+    #endif
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "Browserval", initialProperties: nil, launchOptions: nil)
+    let rootViewController = NSViewController()
+    rootViewController.view = rootView
+
+    popover = NSPopover()
+
+    popover.contentSize = NSSize(width: 700, height: 800)
+    popover.animates = true
+    popover.behavior = .transient
+    popover.contentViewController = rootViewController
+
+    statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(60))
+
+    if let button = self.statusBarItem.button {
+      print("FOO")
+      button.action = #selector(togglePopover(_:))
+      button.title = "TEST"
+    }
+
+  }
+
+  @objc func togglePopover(_ sender: AnyObject?) {
+      if let button = self.statusBarItem.button {
+          if self.popover.isShown {
+              self.popover.performClose(sender)
+          } else {
+              self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+
+              self.popover.contentViewController?.view.window?.becomeKey()
+          }
+      }
+  }
+}
